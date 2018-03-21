de2255bbbef7029e221633e2f230defc7ae37be2
Steve Hobbs <elkdanger@gmail.com>
2018-03-21 21:43:50 +0000

    Add react, component, webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import Logger from './logger';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { App } from './App';
 
 new Logger().log('Hello');
+
+const root = document.getElementById('app');
+
+ReactDOM.render(<App/>, root);
